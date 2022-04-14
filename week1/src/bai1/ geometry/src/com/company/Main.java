81b2d72b1b590d0f26b7db4b07bd3990acea363c
thientai2712 <thienta2712@gmail.com>
2022-04-14 15:23:06 +0700

    update

--- week1/src/bai1/ geometry/src/com/company/Main.java
+++ week1/src/bai1/ geometry/src/com/company/Main.java
@@ -32,16 +32,31 @@ public class Main {
 //         System.out.println(" ");
 //     }
 //}
+//public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//    int height;
+//    System.out.print("Nhập chiều cao: ");
+//    height = scanner.nextInt();
+//    for(int i=height;i>=1;i--){
+//        for (int j=1;j<=i;j++){
+//            System.out.print("* ");
+//        }
+//        System.out.println(" ");
+//    }
+//}
 public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
     int height;
-    System.out.print("Nhập chiều cao: ");
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Nhập độ cao của tam giác: ");
     height = scanner.nextInt();
-    for(int i=height;i>=1;i--){
-        for (int j=1;j<=i;j++){
-            System.out.print("* ");
+    for (int i = 0;i<height;i++){
+        for (int j =0; j<height - i;j++){
+            System.out.print(" ");
         }
-        System.out.println(" ");
+        for (int k = 0 ; k < 2*i-1; k++){
+            System.out.print("+");
+        }
+        System.out.println("");
     }
 }
 }
