9138967249d2fed201f3b4c50d44e9d01677d6f4
thientai2712 <thienta2712@gmail.com>
2022-04-14 14:46:24 +0700

    update

---
+++ week1/src/bai1/ geometry/src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+//    public static void main(String[] args) {
+//        int height ;
+//        int weight ;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Nhập chiều cao: ");
+//        height = scanner.nextInt();
+//        System.out.print("Nhập chiều rộng: ");
+//        weight = scanner.nextInt();
+//        for (int i = 1;i<=height;i++){
+//            for (int j=1;j<=weight;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println(" ");
+//        }
+//
+//    }
+//public static void main(String[] args) {
+//     int height;
+//     Scanner scanner = new Scanner(System.in);
+//    System.out.print("Nhập chiều cao: ");
+//    height = scanner.nextInt();
+//     for (int i=1;i<=height;i++){
+//         for(int j = 1;j<=i;j++){
+//             System.out.print("* ");
+//         }
+//         System.out.println(" ");
+//     }
+//}
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int height;
+    System.out.print("Nhập chiều cao: ");
+    height = scanner.nextInt();
+    for(int i=height;i>=1;i--){
+        for (int j=1;j<=i;j++){
+            System.out.print("* ");
+        }
+        System.out.println(" ");
+    }
+}
+}
